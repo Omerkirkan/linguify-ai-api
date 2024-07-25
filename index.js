b377e87160bb1a1ce217433ce6d641be0a8e9004
omer <krkanomer@yandex.com>
2024-07-25 13:43:03 +0300

    init project

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors'); // cors paketini import edin
+const app = express();
+const port = 3000;
+const { Ollama } = require('ollama');
+
+// JSON body parser
+app.use(express.json());
+
+// CORS middleware'ini ekleyin
+app.use(cors({
+    origin: '*', // İzin verilen domainleri belirleyin
+    methods: 'GET, POST, PUT, DELETE', // İzin verilen HTTP metodları
+    allowedHeaders: 'Content-Type, Authorization' // İzin verilen başlıklar
+}));
+
+const ollama = new Ollama();
+
+// Örnek bir POST endpoint
+app.post('/api/chat', async (req, res) => {
+    try {
+
+        const chatContent = req.body;
+
+        const response = await ollama.chat({ model: 'llama3', messages: chatContent, stream: true })
+        for await (const part of response) {
+            res.write(part.message.content);
+        }
+
+        res.end();
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Sunucu hatası' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Sunucu http://localhost:${port} adresinde çalışıyor.`);
+});
